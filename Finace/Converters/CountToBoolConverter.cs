2a858acbb9ffb3ee0297b45969cf03129b8b1ccb
danevc <devchenko@smart-ts.ru>
2025-11-15 03:52:07 +0500

    кнопки недоступны  если в них нет вложенных записей

---
+++ Finace/Converters/CountToBoolConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace Finace.Converters
+{
+    public class CountToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int count)
+                return count > 0;
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
